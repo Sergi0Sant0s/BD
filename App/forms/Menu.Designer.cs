46f08f4aae0a8430e80ef50ea1f04cfce8415598
Sérgio Santos <sfr_s@outlook.com>
2020-06-11 17:34:13 +0100

    Menu design concluido

---
+++ App/forms/Menu.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace App
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.btnServices = new System.Windows.Forms.Button();
+            this.btnClientes = new System.Windows.Forms.Button();
+            this.btnVeiculos = new System.Windows.Forms.Button();
+            this.btnPecas = new System.Windows.Forms.Button();
+            this.btnFornecedores = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnServices
+            // 
+            this.btnServices.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnServices.FlatAppearance.BorderSize = 0;
+            this.btnServices.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnServices.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnServices.Image = ((System.Drawing.Image)(resources.GetObject("btnServices.Image")));
+            this.btnServices.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnServices.Location = new System.Drawing.Point(84, 134);
+            this.btnServices.Name = "btnServices";
+            this.btnServices.Padding = new System.Windows.Forms.Padding(15, 0, 15, 0);
+            this.btnServices.Size = new System.Drawing.Size(241, 62);
+            this.btnServices.TabIndex = 0;
+            this.btnServices.Text = "Serviços";
+            this.btnServices.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnServices.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnServices.UseVisualStyleBackColor = false;
+            // 
+            // btnClientes
+            // 
+            this.btnClientes.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnClientes.FlatAppearance.BorderSize = 0;
+            this.btnClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClientes.Image = ((System.Drawing.Image)(resources.GetObject("btnClientes.Image")));
+            this.btnClientes.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnClientes.Location = new System.Drawing.Point(84, 66);
+            this.btnClientes.Name = "btnClientes";
+            this.btnClientes.Padding = new System.Windows.Forms.Padding(15, 0, 15, 0);
+            this.btnClientes.Size = new System.Drawing.Size(241, 62);
+            this.btnClientes.TabIndex = 1;
+            this.btnClientes.Text = "Clientes";
+            this.btnClientes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClientes.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnClientes.UseVisualStyleBackColor = false;
+            // 
+            // btnVeiculos
+            // 
+            this.btnVeiculos.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnVeiculos.FlatAppearance.BorderSize = 0;
+            this.btnVeiculos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVeiculos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVeiculos.Image = ((System.Drawing.Image)(resources.GetObject("btnVeiculos.Image")));
+            this.btnVeiculos.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnVeiculos.Location = new System.Drawing.Point(84, 338);
+            this.btnVeiculos.Name = "btnVeiculos";
+            this.btnVeiculos.Padding = new System.Windows.Forms.Padding(15, 0, 15, 0);
+            this.btnVeiculos.Size = new System.Drawing.Size(241, 62);
+            this.btnVeiculos.TabIndex = 2;
+            this.btnVeiculos.Text = "Veiculos";
+            this.btnVeiculos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnVeiculos.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnVeiculos.UseVisualStyleBackColor = false;
+            // 
+            // btnPecas
+            // 
+            this.btnPecas.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnPecas.FlatAppearance.BorderSize = 0;
+            this.btnPecas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPecas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPecas.Image = ((System.Drawing.Image)(resources.GetObject("btnPecas.Image")));
+            this.btnPecas.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnPecas.Location = new System.Drawing.Point(84, 202);
+            this.btnPecas.Name = "btnPecas";
+            this.btnPecas.Padding = new System.Windows.Forms.Padding(15, 0, 15, 0);
+            this.btnPecas.Size = new System.Drawing.Size(241, 62);
+            this.btnPecas.TabIndex = 3;
+            this.btnPecas.Text = "Peças";
+            this.btnPecas.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPecas.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnPecas.UseVisualStyleBackColor = false;
+            // 
+            // btnFornecedores
+            // 
+            this.btnFornecedores.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnFornecedores.FlatAppearance.BorderSize = 0;
+            this.btnFornecedores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFornecedores.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFornecedores.Image = ((System.Drawing.Image)(resources.GetObject("btnFornecedores.Image")));
+            this.btnFornecedores.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnFornecedores.Location = new System.Drawing.Point(84, 270);
+            this.btnFornecedores.Name = "btnFornecedores";
+            this.btnFornecedores.Padding = new System.Windows.Forms.Padding(15, 0, 15, 0);
+            this.btnFornecedores.Size = new System.Drawing.Size(241, 62);
+            this.btnFornecedores.TabIndex = 4;
+            this.btnFornecedores.Text = "Fornecedores";
+            this.btnFornecedores.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFornecedores.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnFornecedores.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnExit.Location = new System.Drawing.Point(84, 406);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Padding = new System.Windows.Forms.Padding(15, 0, 15, 0);
+            this.btnExit.Size = new System.Drawing.Size(241, 62);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Sair";
+            this.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnMin
+            // 
+            this.btnMin.BackColor = System.Drawing.Color.Transparent;
+            this.btnMin.FlatAppearance.BorderSize = 0;
+            this.btnMin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnMin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnMin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMin.Image = ((System.Drawing.Image)(resources.GetObject("btnMin.Image")));
+            this.btnMin.Location = new System.Drawing.Point(969, 12);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(43, 41);
+            this.btnMin.TabIndex = 6;
+            this.btnMin.UseVisualStyleBackColor = false;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1024, 575);
+            this.Controls.Add(this.btnMin);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnFornecedores);
+            this.Controls.Add(this.btnPecas);
+            this.Controls.Add(this.btnVeiculos);
+            this.Controls.Add(this.btnClientes);
+            this.Controls.Add(this.btnServices);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Menu";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Oficina";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Menu_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnServices;
+        private System.Windows.Forms.Button btnClientes;
+        private System.Windows.Forms.Button btnVeiculos;
+        private System.Windows.Forms.Button btnPecas;
+        private System.Windows.Forms.Button btnFornecedores;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnMin;
+    }
+}
+
---
+++ App/forms/Menu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+            Color colDefault = btnClientes.BackColor;
+            int trans = 175;
+            btnClientes.BackColor = Color.FromArgb(trans, colDefault.R, colDefault.G, colDefault.B);
+            btnExit.BackColor = Color.FromArgb(trans, colDefault.R, colDefault.G, colDefault.B);
+            btnFornecedores.BackColor = Color.FromArgb(trans, colDefault.R, colDefault.G, colDefault.B);
+            btnPecas.BackColor = Color.FromArgb(trans, colDefault.R, colDefault.G, colDefault.B);
+            btnServices.BackColor = Color.FromArgb(trans, colDefault.R, colDefault.G, colDefault.B);
+            btnVeiculos.BackColor = Color.FromArgb(trans, colDefault.R, colDefault.G, colDefault.B);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Menu_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+    }
+}
